ca9fd8e0460200855abfc6202e7f78723432fd14
kim hyeon min <102670376+gus11als@users.noreply.github.com>
2022-08-29 19:13:46 +0900

    Update inside_post.py

--- project/inside_post.py
+++ project/inside_post.py
@@ -6,5 +6,5 @@ while True:
     os.system("node inside_post.js")
     time.sleep(13)
     
-    \\ inside_post.js 를 반복해서 돌려주는 파일
-    \\ python3 outside_post.py <-  실행방법
+    // inside_post.js 를 반복해서 돌려주는 파일
+    // python3 outside_post.py <-  실행방법
